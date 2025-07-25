d982e7230d45fede29f51d45e65f2513801aae8e
muhammad abdullah <semester1.mitho@gmail.com>
2025-07-25 19:26:35 +0500

    Add files via upload

--- phonebook.py
+++ phonebook.py
@@ -27,3 +27,25 @@ def display_contact ():
           print("contacts")
           for name,number in phonebook.items():
                print(f"{name}: {number}")
+def main():
+                    while True:
+                         print("/n---phonebook menu----")
+                         print("1:add contact")
+                         print("2:search contact")
+                         print("3:delete contact")
+                         print("4:display all contacts")
+                         print("5:exit")
+                         choice = input("choose an option (1-5):").strip()
+                         if choice =='1':
+                              add_contact()
+                         elif choice =='2':
+                              search_contact()
+                         elif choice =='3':
+                            delete_contact()
+                         elif choice =='4':
+                              display_contact()
+                         elif choice =='5':
+                              print ("exiting phonebook.Goodbye!")
+                              break
+                         print("invalid option.pls try again")
+main()
