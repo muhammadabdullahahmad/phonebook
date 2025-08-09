c32921e5a91a328c78790ac8371497e645c535ac
muhammad abdullah <semester1.mitho@gmail.com>
2025-08-09 10:32:52 +0500

    Create missing App.tsx file

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <h1>Cricket Scoring App</h1>
+    </div>
+  );
+}
+
+export default App;
